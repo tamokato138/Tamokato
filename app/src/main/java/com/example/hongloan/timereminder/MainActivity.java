94eb315496bf3948015d1b5591c86431afc641ae
Hong Loan <loan.tran@smartdev.vn>
2017-01-02 21:48:18 +0700

    custom icon tab

--- app/src/main/java/com/example/hongloan/timereminder/MainActivity.java
+++ app/src/main/java/com/example/hongloan/timereminder/MainActivity.java
@@ -9,17 +9,24 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout tabLayout;
     PagerAdapter adapter;
+    int tabCount = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tab();
+
+    }
+
+    public void tab() {
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 0; i < tabCount; i++) {
             tabLayout.addTab(tabLayout.newTab());
+
         }
-        adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount(), this);
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
     }
--- app/src/main/java/com/example/hongloan/timereminder/PagerAdapter.java
+++ app/src/main/java/com/example/hongloan/timereminder/PagerAdapter.java
@@ -1,8 +1,14 @@
 package com.example.hongloan.timereminder;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 
 import com.example.hongloan.timereminder.Tabs.GroupTaskList;
 import com.example.hongloan.timereminder.Tabs.LocationTab;
@@ -14,15 +20,15 @@ import com.example.hongloan.timereminder.Tabs.WeatherTab;
  */
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
+    Context context;
     int tabCount;
-    public static final String TITLE_TAB_1 = "Task";
-    public static final String TITLE_TAB_2 = "To do";
-    public static final String TITLE_TAB_3 = "Weather";
-    public static final String TITLE_TAB_4 = "Location";
+    public static int[] tabIconId = {R.drawable.ic_plus, R.drawable.ic_justify_paragrap, R.drawable.ic_cloud, R.drawable.ic_location};
 
+//    public static String[] tabTitle = {"Task", "To do", "Weather", "Location"};
 
-    public PagerAdapter(FragmentManager fm, int tabCount) {
+    public PagerAdapter(FragmentManager fm, int tabCount, Context context) {
         super(fm);
+        this.context = context;
         this.tabCount = tabCount;
     }
 
@@ -53,21 +59,13 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
-        String title = "";
-        switch (position) {
-            case 0:
-                title = TITLE_TAB_1;
-                break;
-            case 1:
-                title = TITLE_TAB_2;
-                break;
-            case 2:
-                title = TITLE_TAB_3;
-                break;
-            case 3:
-                title = TITLE_TAB_4;
-                break;
-        }
-        return title;
+        //Cần coi lại
+        Drawable image = ContextCompat.getDrawable(context, tabIconId[position]);
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+        SpannableString sb = new SpannableString(" ");
+        ImageSpan imageSpan = new ImageSpan(image);
+        sb.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        return sb;
     }
+
 }
